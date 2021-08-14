dd95c1932f0d58060c40d50b3704ab0d159567de
Sara Gallego Villada <79612209+SaraGallego22@users.noreply.github.com>
2021-08-14 12:01:35 -0500

    Add files via upload

---
+++ proyecto/codigo/codigo1.py
@@ -0,0 +1,33 @@
+import numpy as np
+import os
+from numpy.core.records import array
+import pandas as pd
+
+#Ganado enfermo
+def leer_csv_enfermo (folder_path,lista_df,names):
+    names = os.listdir(folder_path)
+    print(os.listdir(folder_path))
+    for i in names:   
+        archivo = pd.read_csv(r'''C:\Users\ASUS\OneDrive - Universidad EAFIT\ST0245-Eafit\proyecto\datasets\csv\enfermo_csv\{}'''.format(i))
+        lista_df.append(archivo)
+        #print(lista_df,[0,3])
+    print (lista_df)
+
+#Ganado sano
+def leer_csv_sano (folder_path,lista_df,names):
+    names = os.listdir(folder_path)
+    print(os.listdir(folder_path))
+    for i in names:   
+        archivo = pd.read_csv(r'C:\Users\ASUS\OneDrive - Universidad EAFIT\ST0245-Eafit\proyecto\datasets\csv\sano_csv\{}'.format(i))
+        lista_df.append(archivo)
+        #print(lista_df,[0,3])
+    return lista_df
+
+def __main__():
+    folder_path = r'C:\Users\ASUS\OneDrive - Universidad EAFIT\ST0245-Eafit\proyecto\datasets\csv\enfermo_csv'
+    names = []
+    files = []
+    leer_csv_enfermo(folder_path, files, names)
+   
+__main__()
+
